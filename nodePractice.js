8144f2ccd0bbda7b98a6b19de35e048cbaeefc38
Tristan Wakefield <tristan.wakefield@yahoo.com>
2024-10-07 10:29:18 -0500

    partial

--- nodePractice.js
+++ nodePractice.js
@@ -1,2 +1,10 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js");
+const name = getInput(n)
+console.log('Hello ' + name) 
+//const cap = "is your name capitalized"
+//console.log(name + cap)
+
+
+//toUpperCase()
+//toLowerCase()
